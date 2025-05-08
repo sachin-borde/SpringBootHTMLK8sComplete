bb3a734f5b894cdf00fba09b417995939216372a
sachin-borde <ssborde26@gmail.com>
2025-05-08 10:34:54 +0530

    comment

---
+++ src/main/java/com/main/SpringBootHTMLK8sComplete.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHTMLK8sComplete {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHTMLK8sComplete.class, args);
+	}
+
+}
